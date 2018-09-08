be25440a78109108290d5762efc5723a372ac44a
Juhnowski <juhhnowski@gmail.com>
2018-09-09 02:13:58 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let x = 5;
+
+    if true {
+        let a = 1;
+        let x = 1;
+        println!("a={}", a);
+        println!("x={}", x);
+    }
+
+    println!("x={}", x);
+    //println!("a={}", a);
+}
